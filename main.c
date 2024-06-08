b8386f080b4f36eccf35a494538eb5ddbdd47764
misachi <misachibrian@gmail.com>
2024-06-08 20:32:10 +0000

    Add commands for accessing the hash map data
    Add types for values for hash map

--- main.c
+++ main.c
@@ -5,18 +5,37 @@
 
 #include "MurmurHash3.h"
 
-#define LOAD_FACTOR (float)(0.5)  // 0-100
+#define LOAD_FACTOR (float)(0.85) // 0-100
 #define MAXIMUM_SIZE 1073741824UL // maximum limit of hashmap; default 1GB
 // #define EMPTY (uint64_t)18446744073709551616
 #define EVICT 1
 #define RESIZE_POLICY 2
 #define EMPTY (int8_t)(-1)
 #define TOMBSTONE NULL
+#define SUCCESS (void *)-1
+
+typedef enum
+{
+    CMD_SET,
+    CMD_GET,
+    CMD_PUT,
+    CMD_DEL,
+    CMD_NOOP
+} KV_CMD;
+typedef enum
+{
+    KV_INT16,
+    KV_INT32,
+    KV_INT64,
+    KV_FLOAT,
+    KV_DOUBLE,
+    KV_STRING
+} KV_TYPE;
 
 struct hash_map;
 typedef uint32_t (*hash_function)(const void *key, int len, int seed);
 
-struct hash_map *KV_init(unsigned long size, hash_function hash_fn);
+struct hash_map *KV_init(unsigned long size, hash_function hash_fn, KV_TYPE val_type);
 int KV_set(struct hash_map *hmap, char *key, int key_len, char *val, int val_len);
 void *KV_get(struct hash_map *hmap, char *key, int key_len);
 int KV_delete(struct hash_map *hmap, char *key, int key_len);
@@ -39,6 +58,7 @@ struct hash_map
     int len;
     int capacity;
     int seed;
+    KV_TYPE val_type;
 #ifdef ALLOW_STATS
     uint64_t misses;
     uint64_t hits;
@@ -47,6 +67,108 @@ struct hash_map
     char *arr; // array elements
     hash_function hash_fn;
 };
+
+struct hash_map *HMAP = NULL;
+
+size_t get_type_size(KV_TYPE val_type, char *val)
+{
+    switch (val_type)
+    {
+    case KV_INT16:
+        return sizeof(int16_t);
+    case KV_INT32:
+        return sizeof(int32_t);
+    case KV_INT64:
+        return sizeof(int64_t);
+    case KV_FLOAT:
+        return sizeof(float);
+    case KV_DOUBLE:
+        return sizeof(KV_DOUBLE);
+    case KV_STRING:
+        return strlen(val);
+    default:
+        break;
+    }
+    return -1;
+}
+
+KV_CMD parse_cmd(char *cmd, int len)
+{
+    if (memcmp("SET", cmd, len) == 0)
+    {
+        return CMD_SET;
+    }
+    else if (memcmp("GET", cmd, len) == 0)
+    {
+        return CMD_GET;
+    }
+    else if (memcmp("PUT", cmd, len) == 0)
+    {
+        return CMD_PUT;
+    }
+    else if (memcmp("DEL", cmd, len) == 0)
+    {
+        return CMD_DEL;
+    }
+    else
+    {
+        return CMD_NOOP;
+    }
+}
+
+void *process_cmd(struct hash_map *hmap, int argc, char *argv[])
+{
+    if (argc < 1)
+    {
+        printf("Command is required\n");
+        exit(1);
+    }
+
+    char *cmd = argv[0];
+    int len = strlen(argv[0]);
+    int ret;
+
+    switch (parse_cmd(cmd, len))
+    {
+    case CMD_SET:
+    case CMD_PUT:
+        if (argc < 3)
+        {
+            printf("SET Error: Value was not provided\n");
+            return NULL;
+        }
+
+        ret = KV_set(hmap, argv[1], strlen(argv[1]), argv[2], get_type_size(hmap->val_type, argv[2]));
+        if (ret == 0)
+        {
+            return SUCCESS;
+        }
+        break;
+    case CMD_GET:
+        if (argc < 2)
+        {
+            printf("GET Error: Key was not provided\n");
+            break;
+        }
+        return KV_get(hmap, argv[1], strlen(argv[1]));
+    case CMD_DEL:
+        if (argc < 2)
+        {
+            printf("DELETE Error: Key was not provided\n");
+            break;
+        }
+        ret = KV_delete(hmap, argv[1], strlen(argv[1]));
+        if (ret == 0)
+        {
+            return SUCCESS;
+        }
+        break;
+    default:
+        printf("Invalid command\n");
+        break;
+    }
+    return NULL;
+}
 
 uint32_t first_slot(uint32_t hash, int capacity)
 {
@@ -129,7 +251,7 @@ void hash_map_resize(struct hash_map *hmap, int policy)
     hmap->arr = buf;
 }
 
-struct hash_map *KV_init(unsigned long capacity, hash_function hash_fn)
+struct hash_map *KV_init(unsigned long capacity, hash_function hash_fn, KV_TYPE val_type)
 {
     struct hash_map *hmap = (struct hash_map *)malloc(sizeof(struct hash_map));
     if (hmap == NULL)
@@ -152,6 +274,7 @@ struct hash_map *KV_init(unsigned long capacity, hash_function hash_fn)
     hmap->size = capacity * sizeof(struct KV);
     hmap->seed = 1;
     hmap->capacity = capacity;
+    hmap->val_type = val_type;
     return hmap;
 }
 
@@ -224,13 +347,11 @@ int KV_set(struct hash_map *hmap, char *key, int key_len, char *val, int val_len
 {
     size_t size;
     int ret;
-    struct KV *entry = NULL;
 
-    // size = key_len + val_len + sizeof(struct KV);
     int slot = find_empty_slot(hmap, key, key_len);
 
     // KV already allocated during initialization. We just need to get our KV chunk
-    entry = (struct KV *)&hmap->arr[slot * sizeof(struct KV)];
+    struct KV *entry = (struct KV *)&hmap->arr[slot * sizeof(struct KV)];
 
     if (*(int8_t *)entry == EMPTY)
     {
@@ -310,9 +431,9 @@ void *KV_get(struct hash_map *hmap, char *key, int key_len)
 {
     int64_t slot = find(hmap, key, key_len);
     struct KV *entry = NULL;
-    if (slot <= -1)
+    if (slot == -1)
     {
-        return (void *)-1;
+        return NULL;
     }
     entry = (struct KV *)&hmap->arr[slot * sizeof(struct KV)];
     return entry->val;
@@ -364,33 +485,31 @@ int set_int_val(struct hash_map *hmap, char *key, int key_len, int val)
 int main(int argc, char *argv[])
 {
     char *keys[] = {"foo", "bar", "noop", "foo1", "kai", "boop", "baruuituityuy", "Tosha"};
-    int vals[] = {100, 200, 300, 400, 500, 600, 700, 800};
+    int16_t vals[] = {100, 200, 300, 400, 500, 600, 700, 800};
 
-    // printf("%ld\n\n\n", sizeof(keys));
-
-    // for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++)
-    // {
-    // uint32_t hash = KV_hash_function(keys[i], sizeof(keys[i]), 1);
-    // printf("%d\n", hash);
-    // printf("%d\n", hash % 10);
-    // }
-    struct hash_map *hmap = KV_init(4, KV_hash_function);
+    struct hash_map *hmap = KV_init(4, KV_hash_function, KV_INT16);
 
     for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++)
     {
-        set_int_val(hmap, keys[i], sizeof(keys[i]), vals[i]);
+        char *v[] = {"SET", keys[i], (char *)&vals[i]};
+        process_cmd(hmap, 3, v);
+        // set_int_val(hmap, keys[i], strlen(keys[i]), vals[i]);
     }
 
+    char *v[] = {"DEL", "bar"};
+    process_cmd(hmap, 2, v);
+
     for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++)
     {
-        char *ret = (char *)KV_get(hmap, keys[i], sizeof(keys[i]));
-        if (ret == (void *)-1)
+        char *v[] = {"GET", keys[i]};
+        char *ret = process_cmd(hmap, 2, v);
+        if (ret == NULL)
         {
             printf("Not found\n");
         }
         else
         {
-            printf("%s: %d\n", keys[i], *(int *)ret);
+            printf("%s: %d\n", keys[i], *(int16_t *)ret);
         }
     }
 
